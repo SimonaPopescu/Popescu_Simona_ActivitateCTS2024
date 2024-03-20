333542194bbace16b057c213d9429f1b615f8f34
SimonaPopescu <popescusimona21@stud.ase.ro>
2024-03-20 21:20:16 +0200

    Seminar 5 commit

---
+++ Seminar5/src/Main.java
@@ -0,0 +1,19 @@
+import builder_v1.Pacient;
+import builder_v1.PacientBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Pacient pacient= new PacientBuilder().setPatRabatabil(true).setPapuciDeCamera(true).setMicDejunInclus(true).setHalatPentruInterior(true).build();
+
+        Pacient pacient2= new PacientBuilder().setPatRabatabil(true).build();
+
+        System.out.println(pacient);
+        System.out.println(pacient2);
+
+        builder_v3.Pacient.PacientBuilder builder= new builder_v3.Pacient.PacientBuilder();
+
+        builder_v3.Pacient pacient3= builder.setHalatPentruInterior(true).setPapuciDeCamera(false).setMicDejunInclus(true).setPatRabatabil(true).build();
+
+        System.out.println(pacient3.toString());
+    }
+}
---
+++ Seminar5/src/builder_v1/Builder.java
@@ -0,0 +1,6 @@
+package builder_v1;
+
+public interface Builder {
+
+    public Pacient build();
+}
